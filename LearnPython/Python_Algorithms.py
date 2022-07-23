dc14e1ab2a22bc99884d2e7a355d0d17bdbf3e64
kampf <matthew196580@gmail.com>
2022-07-23 14:36:22 -0500

    Created Python_Algo file

---
+++ LearnPython/Python_Algorithms.py
@@ -0,0 +1,26 @@
+# Python Algorithms
+
+# An algorithm is best described as a set of instructions or rules expressed in a step-by-step fashion that
+# represents the procedure to solve the problem to reach the required to be output is called an Algorithm - the best
+# analogy is a recipe. Algorithms are analyzed and ranked on their efficiency using the big O notation.
+
+# Below is an example of an algorithm to add two numbers together, this algorithm serves an incredibly specific and
+# tailored role in the code - to add two numbers together, and it's quite good at the task.
+
+def add_numbers():
+    input1 = input("Enter the first number: ")
+    input2 = input("Enter the second number: ")
+    print(int(input1) + int(input2))
+
+
+add_numbers()
+
+# As you can imagine, algorithms are a fundamental piece of programming as they're used to solve problems given a set
+# of instructions, simply put - an algorithm should be designed to solve a specific problem in the code efficiently.
+# I highly recommend you read deeper into the resources I link on the wiki as this is a very valuable topic.
+
+# Create your first algorithm to take a user's input then multiply the numbers together and print it to the screen,
+# you may use the above add_numbers algorithm as an example.
+
+
+# END of Python Algorithms
